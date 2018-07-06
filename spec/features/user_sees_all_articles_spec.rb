18f7e97167bc2119c6f2c30641979510be18faab
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-07-05 20:06:17 -0600

    add test to see one article

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -3,15 +3,14 @@ require "rails_helper"
 describe "user sees all articles" do
   describe "they visit /articles" do
     it "displays all articles" do
-      article_1 = Article.create!(title: "Title 1", body: "Body 1")
-      article_2 = Article.create!(title: "Title 2", body: "Body 2")
+      article_1 = Article.create(title: "Title 1", body: "Body 1")
+      article_2 = Article.create(title: "Title 2", body: "Body 2")
 
       visit '/articles'
 
       expect(page).to have_link(article_1.title)
       expect(page).to have_link(article_2.title)
       expect(page).to have_link("Create a New Article")
-
     end
   end
 end
